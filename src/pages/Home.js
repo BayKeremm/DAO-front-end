7d1f59f9876b915ed69072fddbcee3d613ef030f
Kerem Okyay <82160210+BayKeremm@users.noreply.github.com>
2022-07-22 13:50:44 +0200

    can create proposal and sponsor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,62 +6,115 @@ import { ethers } from "ethers";
 import molochABI from "../abis/Moloch.json"
 import tokenABI from "../abis/Token.json"
 
+import {createClient} from 'urql'
+
+const APIURL = "http://f587-54-87-32-29.ngrok.io/subgraphs/name/daodesigner/moloch-subgraph"
+const query = `
+query MyQuery {
+  moloch(id: "0x3155755b79aa083bd953911c92705b7aa82a18f9") {
+	id
+	proposals {
+  	proposalId
+  	sponsored
+    details
+	}
+  }
+}
+`
+const client = createClient({
+  url:APIURL
+})
+
+// addresses on aws
+const molochAddress = "0x3155755b79aa083bd953911c92705b7aa82a18f9";
+const tokenAddress = "0x3347b4d90ebe72befb30444c9966b2b990ae9fcb";
 
 async function createProposal(details){
-  const summoner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-  const account3 = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
-  const molochAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-  const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const moloch = new ethers.Contract(molochAddress,molochABI.abi,provider);
-  const token = new ethers.Contract(tokenAddress,tokenABI.abi,provider);
-  // check allowance
-  const res = await token.allowance(summoner,molochAddress);
-  console.log(res._hex);
-  if(res._hex === "0x00"){
-    console.log("not enough allowance")
-    return
-  }
-  console.log("after")
-  //const totalSupply = await token.totalSupply();
-  //console.log("fetched token and copied token",totalSupply);
+  const  signer = provider.getSigner();
+  const moloch = new ethers.Contract(molochAddress,molochABI.abi,signer);
+  const address = await signer.getAddress();
+  // create proposal
+  //address applicant,
+  //uint256 sharesRequested,
+  //uint256 lootRequested,
+  //uint256 tributeOffered,
+  //address tributeToken,
+  //uint256 paymentRequested,
+  //address paymentToken,
+  //string memory details
+  await moloch.submitProposal(address,0,0,1,tokenAddress,0,tokenAddress,details)
+  console.log("proposal submitted");
+}
 
+async function increaseAllowance(){
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const  signer = await provider.getSigner();
+  const token = new ethers.Contract(tokenAddress,tokenABI.abi,signer);
+
+  const res = await token.increaseAllowance(molochAddress,5000);
+  //console.log(res);
+  console.log("increased allowance")
 }
 
 const Home = () => {
   const [proposals, setProposals] = useState([
     [
-      1,
+      0,
       <div>Should we start a Moralis hamburger chain?</div>,
+      <Link to="/proposal" state={0}>
       <Tag color="green" text="Passed" />,
+      </Link>,
     ],
     [
-      2,
+      1,
       "Should we accept Elon Musks $44billion offer for our DAO?",
-      <Link to="/proposal" state={"hello"}>
+      <Link to="/proposal" state={1}>
         <Tag color="red" text="Rejected" />
       </Link>,
     ],
     [
-      3,
+      2,
       "Do you want a Web3 Slack tutorial?",
+      <Link to="/proposal" state={2}>
       <Tag color="blue" text="Ongoing" />,
+      </Link>,
+    ],
+    [
+      3,
+      "Are you interested in Xbox/Console web3 tutorials?",
+      <Link to="/proposal" state={3}>
+      <Tag color="blue" text="Ongoing" />,
+      </Link>,
     ],
     [
       4,
-      "Are you interested in Xbox/Console web3 tutorials?",
-      <Tag color="blue" text="Ongoing" />,
-    ],
-    [
-      5,
       "Would you attend a Moralis Builder get together in Miami?",
+      <Link to="/proposal" state={4}>
       <Tag color="blue" text="Ongoing" />,
+      </Link>,
     ],
 ]);
 
+const [datas, setDatas] = useState([])
+useEffect(()=>{
+  fetchData()
+},[]);
 
-  
+async function fetchData(){
+  const response = await client.query(query).toPromise()
+  console.log('response',response.data.moloch.proposals)
+  //setDatas(response.data.moloch.proposals)
+  const table = response.data.moloch.proposals.map((e) => [
+      e.proposalId,
+      e.details,
+      <Link to="/proposal" state={e.proposalId}>
+      <Tag color="blue" text={e.sponsored ? "yes":"no"} />,
+      </Link>,
+    ]);
+  setDatas(table)
+    
+}
 
   return (
     <>
@@ -72,19 +125,28 @@ const Home = () => {
               Recent Proposals
               <div style={{marginTop: "30px"}}>
                 <Table
-                columnsConfig="10% 70% 20%"
-                data={proposals}
+                columnsConfig="40% 40% 40%"
+                data={datas}
                 header={[
                   <span>ID</span>,
-                  <span>Description</span>,
-                  <span>Status</span>,
+                  <span>DETAILS</span>,
+                  <span>SPONSORED</span>,
                 ]}
                 pageSize={5}
                 />
               </div>
+
               <Button
-                //onClick={function noRefCheck(){}}
-                text="Button"
+                onClick={(e)=> {
+                  try{
+                    increaseAllowance();
+
+                  }catch(e){
+                    console.log(e);
+
+                  }
+                }}
+                text="Increase Allowance by 5000"
               />
 
               <Form
@@ -109,7 +171,7 @@ const Home = () => {
                     try{
                       createProposal(e.data[0].inputResult);
                     } catch(e){
-
+                      console.log(e);
                     }
                     
                   }}
--- src/pages/Proposal.js
+++ src/pages/Proposal.js
@@ -1,10 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "./pages.css";
-import { Tag, Widget, Blockie, Tooltip, Icon, Form, Table } from "web3uikit";
-import { Link } from "react-router-dom";
+import { Tag, Widget, Blockie, Tooltip, Icon, Form, Table, Button } from "web3uikit";
+import { Link,useLocation } from "react-router-dom";
+import { ethers } from "ethers";
+import molochABI from "../abis/Moloch.json"
+import tokenABI from "../abis/Token.json"
+
+
+// addresses on aws
+const molochAddress = "0x3155755b79aa083bd953911c92705b7aa82a18f9";
+const tokenAddress = "0x3347b4d90ebe72befb30444c9966b2b990ae9fcb";
+
+async function sponsorProposal(data){
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const  signer = provider.getSigner();
+  const moloch = new ethers.Contract(molochAddress,molochABI.abi,signer);
+  const address = await signer.getAddress();
+  // get proposal id
+  // check if member
+  // check if allowance is there
+  await moloch.sponsorProposal(data);
+  console.log("proposal sponsored");
+}
+ async function castVote(vote,data){
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const  signer = provider.getSigner();
+  const moloch = new ethers.Contract(molochAddress,molochABI.abi,signer);
+  const address = await signer.getAddress();
+
+  // check if it is sponsored this can be done when fetching the data
+
+  if(vote===true){
+    await moloch.submitVote(data,1) // proposalIndex, uintVote
+
+  }else{
+    await moloch.submitVote(data,0) // proposalIndex, uintVote
+  }
+  console.log("proposal voted");
+ } 
 
 const Proposal = () => {
-
+  const location = useLocation();
+  const data = location.state;
   return (
     <>
       <div className="contentProposal">
@@ -26,6 +63,20 @@ const Proposal = () => {
             </div>
           </div>
         </div>
+
+          <Button
+            onClick={(e)=> {
+              try{
+                //console.log(data);
+                sponsorProposal(data);
+
+              }catch(e){
+                console.log(e);
+
+              }
+            }}
+            text="Sponsor proposal"
+          />
           <Form
             style={{
               width: "35%",
@@ -51,9 +102,9 @@ const Proposal = () => {
             ]}
             onSubmit={(e) => {
               if (e.data[0].inputResult[0] === "For") {
-                //castVote(true);
+                castVote(true,data);
               } else {
-                //castVote(false);
+                castVote(false,data);
               }
               //setSub(true);
             }}
